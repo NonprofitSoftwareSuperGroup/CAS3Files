0695dd18db381bd064357476eeffa695a405b13d
pyromancer2 <petekeller2@gmail.com>
2014-04-18 17:13:31 -0400

    Database

--- cas3database .sql
+++ cas3database .sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.9
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
--- Host: localhost:8889
--- Generation Time: Apr 18, 2014 at 05:47 AM
--- Server version: 5.5.34
--- PHP Version: 5.5.10
+-- Host: 127.0.0.1
+-- Generation Time: Apr 18, 2014 at 11:12 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `cas3database`
@@ -17,37 +23,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courseotkmatch`
---
-
-CREATE TABLE `courseotkmatch` (
-  `course` text COLLATE utf8_unicode_ci NOT NULL,
-  `otk` text COLLATE utf8_unicode_ci,
-  `otk2` text COLLATE utf8_unicode_ci,
-  `otk3` text COLLATE utf8_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Used to check if OTK matches course';
-
---
--- Dumping data for table `courseotkmatch`
---
-
-INSERT INTO `courseotkmatch` (`course`, `otk`, `otk2`, `otk3`) VALUES
-('CMPT-280', 'ABC', 'otk2', NULL),
-('CMPT-281', 'password', NULL, NULL),
-('CMPT-371', 'ABC', NULL, NULL),
-('CMPT-372', 'new', 'abc', 'text'),
-('CMPT-280', 'ABC', 'otk2', NULL),
-('CMPT-281', 'password', NULL, NULL),
-('CMPT-371', 'ABC', NULL, NULL),
-('CMPT-372', 'new', 'abc', 'text');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `professor`
 --
 
-CREATE TABLE `professor` (
+CREATE TABLE IF NOT EXISTS `professor` (
   `username` text COLLATE utf8_unicode_ci NOT NULL,
   `password` text COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -68,11 +47,12 @@ INSERT INTO `professor` (`username`, `password`) VALUES
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
+CREATE TABLE IF NOT EXISTS `students` (
   `index` double NOT NULL AUTO_INCREMENT,
   `email` varchar(30) NOT NULL,
   `otk` varchar(30) NOT NULL,
   `used` varchar(5) NOT NULL,
+  `course` text NOT NULL,
   PRIMARY KEY (`index`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
@@ -80,9 +60,13 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`index`, `email`, `otk`, `used`) VALUES
-(15, 'mike@aol.com', '9175', 'NO'),
-(16, 'matt@aol.com', '7315', 'NO'),
-(17, 'joe@aol.com', '3281', 'NO'),
-(18, 'peter@aol.com', '3816', 'NO'),
-(19, 'sam@aol.com', '5640', 'NO');
+INSERT INTO `students` (`index`, `email`, `otk`, `used`, `course`) VALUES
+(15, 'mike@aol.com', '9175', 'NO', 'CMPT-280'),
+(16, 'matt@aol.com', '7315', 'NO', 'CMPT-281'),
+(17, 'joe@aol.com', '3281', 'NO', 'CMPT-280'),
+(18, 'peter@aol.com', '3816', 'NO', 'CMPT-371'),
+(19, 'sam@aol.com', '5640', 'NO', 'CMPT-372');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
