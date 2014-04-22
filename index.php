445d75cbdb503bee3713a13b8b15551862f52614
estwanick <estwanick@gmail.com>
2014-04-21 23:36:23 -0400

    house keeping

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <!--  Please check out Foundation 5 framework if you plan on changing any of the visuals -->
 
 <div class="row">
-  <div class="small-6 large-2 columns"> Left part of the grid
+  <div class="small-6 large-2 columns">  Left part of the grid 
 
   
 
@@ -18,7 +18,7 @@
   <form action="index.php" name="profForm" method="POST">
 
   <div class="row">
-    <div class="large-6 columns">
+    <div class="large-8 columns">
       <label>Professor login
         <input type="text" name="username" placeholder="Username" />
       </label>
@@ -34,9 +34,9 @@
   <form action="index.php" name="studentForm" method="POST">
 
   <div class="row">
-    <div class="large-4 columns">
+    <div class="large-8 columns">
       <label>Student Login
-        <input type="password" name="otk" placeholder="OneTimeKey" />
+        <input type="password" name="otk" placeholder="OneTimeKey" class="expanded" />
       </label>
 
     </div>
@@ -44,13 +44,22 @@
 
 
   <div class="row">
-    <div class="large-6 columns">
+    <div class="large-8 columns">
       <label>Course Selection
         <select name="courseSelect"> 
-          <option value="CMPT-280!1">CMPT-280-1</option>
-          <option value="CMPT-281!2">CMPT-281-2</option>
-          <option value="CMPT-371!1">CMPT-371-1</option>
-          <option value="CMPT-372!1">CMPT-372-1</option>
+          <option value="CMPT-280">CMPT-280</option>
+          <option value="CMPT-281">CMPT-281</option>
+          <option value="CMPT-371">CMPT-371</option>
+          <option value="CMPT-372">CMPT-372</option>
+        </select>
+      </label>
+      <label>Course Selection
+        <select name="courseSelect"> 
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
         </select>
       </label>
       <input class="button expand alert" type="submit" value="Login" />
